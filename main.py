4748728536ad2b992ed07a5c554a49f5efef08ba
mohsenrahmdeli <mohsenrahmdeli@gmail.com>
2024-04-17 09:45:01 +0400

    Initialize Repository

---
+++ main.py
@@ -0,0 +1,24 @@
+import subprocess
+
+command_one = "netsh wlan show profiles"
+var = "All Usr Profile"
+file_obj = open("req.txt", "w")
+
+a = subprocess.check_output(command_one.split(" ")).decode("utf-8").split('\n')
+a = [i for i in a if "All User Profile" in i]
+
+for i in a:
+    try:
+        t = i.split(':')[1].replace('\n', '')
+        t = t.strip()
+        
+        result = subprocess.check_output(['netsh', 'wlan' , 'show' , 'profiles' , t , 'key=clear']).decode('utf-8').split('\n')
+        result = [b for b in result if "Key Content" in b]
+        
+        end = str(result).split("Key Content")[1].strip()
+        
+        file_obj.write(t+end+'\n')
+        print(t,end)
+    except:
+        None
+file_obj.close()            
